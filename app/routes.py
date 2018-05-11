96b15f55d333ee73918bb8f2a637b66bc176a04c
Frances Sin <frances.sin31@gmail.com>
2018-05-11 15:34:49 -0700

    Course can be selected by user and validated (refactor needed in routes.py)

--- app/routes.py
+++ app/routes.py
@@ -40,12 +40,43 @@ def index ():
         year = re.search ('[0-9]{4}', form.sessions.data).group (0)
         term = re.search ('[S|W]', form.sessions.data).group (0)
         session_href = '/cs/main?sessyr=' + year + '&sesscd=' + term
-        
         active_session = br.find_all ("li", {"class":"active"})[2].find ('a')
         active_session ['href'] = session_href
         br.open (ssc.base_url + active_session ['href'])
         br.open (ssc.browse_url)
+        
+        subject_codes = br.find_all ("td")
+        for s in subject_codes:
+            subject_code_data = s.findAll (text = True)
+            if (len (subject_code_data) == 3):
+                if (subject_code_data [1] == form.subject_code.data):
+                    br.open (ssc.base_url + s.find ('a') ['href']) # subject exists
+                    courses = br.find_all ("td")
+                    course_request = form.subject_code.data + " " + form.course_num.data
+                    for course in courses:
+                        if (course.findAll (text = True) [0] == course_request):
+                            br.open (ssc.base_url + course.find ('a') ['href']) # course exists
+                            sections = br.find_all ("td")
+                            for section in sections:
+                                if (len (section.findAll (text = True)) > 1):
+                                    course_request = form.subject_code.data + " " + form.course_num.data + " " + form.section.data
+                                    if (section.findAll (text = True) [1].strip () == course_request):
+                                        br.open (ssc.base_url + section.find ('a') ['href'])
+                                        print ("reached here.")
+                                        course_data = br.find_all ("td")
+                                        for index, d in enumerate (course_data):
+                                            if (d.findAll (text = True) [0] == 'Total Seats Remaining:'):
+                                                print (int (course_data [index + 1].findAll (text = True) [0]))
+        
+        return redirect (url_for ('index'))
     return render_template ('index.html', title='UBC Course Registrator', form=form)
+
+@app.route ('/results')
+def results ():
+    global br
+    
+    
+    return None
 
 
 
